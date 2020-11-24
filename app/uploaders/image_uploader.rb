b7213402756af97e8c9475bc436091efa549c87d
Edem Dorfe <dayonafi@gmail.com>
2020-11-24 14:13:43 +0900

    fix gem, fix confirm, resize image, delete comment

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [400, 500]
+  version :thumb do
+    process resize_to_fill: [100,75]
+  end
 
   # Choose what kind of storage to use for this uploader:
   storage :file
