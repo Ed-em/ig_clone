9f7649c15e254872bf08d6b7f6c0e1d58d1d7d1c
Edem Dorfe <dayonafi@gmail.com>
2020-11-27 11:09:58 +0900

    bootstrap added

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,4 @@
 class FavoritesController < ApplicationController
-  def index
-    @favorites = Favorite.all
-  end
   def create
     favorite = current_user.favorites.create(feed_id: params[:feed_id])
     redirect_to feeds_url, notice: "liked #{favorite.feed.user.name}'s post'"
