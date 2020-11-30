4c3643a7810d112c9c7a93a2dc4a4866ed5f3105
Edem Dorfe <dayonafi@gmail.com>
2020-11-30 19:43:51 +0900

    user favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,8 @@
 class FavoritesController < ApplicationController
   def index
-    @favorites = Favorite.all
+    favorite = Favorite.all
+    favorite = Favorite.where(user_id: current_user.id).order(created_at: :desc).pluck(:feed_id)
+    @favorites = Feed.find(favorite)
   end
   def create
     favorite = current_user.favorites.create(feed_id: params[:feed_id])
